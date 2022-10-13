3790a3247e9fc6df3ad584c9b668a4757f00a95a
Samuel Gaddis <siddag14@live.com>
2022-10-12 23:02:16 -0400

    commit tree 7c99df094ea2a4a45c9e78886b471373fb6d13cd

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun());
+console.log(saturdayFun('go to sleep'));
+
+
+// THIS IS ANOTHER EX ON HOW TO WRITE UP THE TESTING
+// function saturdayFun(activity) {
+//     if (activity == undefined) {
+//         activity = 'roller-skate'
+//     } 
+//     return `This Saturday, I want to ${activity}!`;
+// }
+
+// console.log(saturdayFun());
+// console.log(saturdayFun('go to sleep'));
+
+function mondayWork(office = 'go to the office') {
+    return `This Monday, I will ${office}.`;
+}
+console.log(mondayWork());
+console.log(mondayWork('go to sleep')); 
+
+function wrapAdjective(visualFlare) {
+   return function(adjective = 'special') {
+        return `You are ${visualFlare}${adjective}${visualFlare}!`;
+}
+}
+
+
